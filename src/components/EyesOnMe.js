c66ca1ab433d17c3fdc291acc60cd6dff33ada1e
MrWilliamA <will.archer@hotmail.com>
2022-05-04 14:32:03 +1000

    fin

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+
+    function focused(){
+        console.log('Good!');
+    }
+
+    function blurred() {
+        console.log('Hey! Eyes on me!');
+    }
+
+  return (
+    <button onFocus={focused} onBlur={blurred}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe;
